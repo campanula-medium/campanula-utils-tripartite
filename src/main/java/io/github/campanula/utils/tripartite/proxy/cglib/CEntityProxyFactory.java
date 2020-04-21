d6afc4a63313108008b67af06ac0997f2e59a4e0
lizelin <liweiyi@91jinrong.com>
2020-04-21 10:27:51 +0800

    fix: 修改参数

--- src/main/java/io/github/campanula/utils/tripartite/proxy/cglib/CEntityProxyFactory.java
+++ src/main/java/io/github/campanula/utils/tripartite/proxy/cglib/CEntityProxyFactory.java
@@ -16,32 +16,32 @@ public class CEntityProxyFactory {
         return new CEntityProxyFactory();
     }
 
-    public T proxy(T entity) {
-        return proxyPlus(entity, entityInterfaces, null, null);
+    public <T> T proxy(T entity) {
+        return proxyPlus(entity, null, null);
     }
 
-    public T proxyBeforePlus(T entity, CAbstractBeforeProxyHandle<T> before) {
-        return proxyPlus(entity, entityInterfaces, before, null);
+    public <T> T proxyBeforePlus(T entity, CAbstractBeforeProxyHandle<T> before) {
+        return proxyPlus(entity, before, null);
     }
 
-    public T proxyAfterPlus(T entity, CAbstractAfterProxyHandle<T> after) {
-        return proxyPlus(entity, entityInterfaces, null, after);
+    public <T> T proxyAfterPlus(T entity, CAbstractAfterProxyHandle<T> after) {
+        return proxyPlus(entity, null, after);
     }
 
-    public T proxyPlus(T entity, CAbstractBeforeProxyHandle<T> before, CAbstractAfterProxyHandle<T> after) {
-        return getProxy(entity, entityInterfaces, before, after);
+    public <T> T proxyPlus(T entity, CAbstractBeforeProxyHandle<T> before, CAbstractAfterProxyHandle<T> after) {
+        return getProxy(entity, before, after);
     }
 
     public <T> List<T> proxy(List<T> entity) {
-        return proxyPlusList(entity, entityInterfaces, null, null);
+        return proxyPlusList(entity, null, null);
     }
 
     public <T> List<T> proxyBeforePlus(List<T> entity, CAbstractBeforeProxyHandle<T> before) {
-        return proxyPlusList(entity, entityInterfaces, before, null);
+        return proxyPlusList(entity, before, null);
     }
 
     public <T> List<T> proxyAfterPlus(List<T> entity, CAbstractAfterProxyHandle<T> after) {
-        return proxyPlusList(entity, entityInterfaces, null, after);
+        return proxyPlusList(entity, null, after);
     }
 
     public <T> List<T> proxyPlusList(List<T> entity, final CAbstractBeforeProxyHandle<T> before, final CAbstractAfterProxyHandle<T> after) {
@@ -54,7 +54,7 @@ public class CEntityProxyFactory {
         Enhancer enhancer = new Enhancer();
         enhancer.setClassLoader(aClass.getClassLoader());
         enhancer.setSuperclass(aClass);
-        enhancer.setClassLoader(new CInvocationHandler<T>(t, before, after));
-        return (I) enhancer.create();
+        enhancer.setCallback(new CInvocationHandler<T>(t, before, after));
+        return (T) enhancer.create();
     }
 }
